1841724b760d5c94db5f6dd0d5dbc8c52e69deb5
Kuldeep Chouhan <kuldeepchouhan42648@GMAIL.COM>
2025-05-25 20:11:26 +0530

    port

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ import flet as ft # type: ignore
 from main import main
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 8550))
     ft.app(target=main, host="0.0.0.0", port=port, view=ft.WEB_BROWSER, assets_dir='assets')
